9091318f3ee9605c6cd29776606c3bad674a4275
Luke Howe-Kerr <luke.howekerr@gmail.com>
2013-12-01 21:04:46 -0500

    It only pops up with one error (on line 59), but I don't understand what that error means. Otherwise, this program is up to date on what you had me work on, including both struct and vectors.

--- main.cpp
+++ main.cpp
@@ -1,32 +1,81 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "luke.h"
 
 using namespace std;
 
+struct order {
+    vector<string> list;
+    vector<double> listprices;
+    string gmessage;
+} order1;
+
 int main (int argc, char *argv[])
 {
-    double registerCash, price, cash, change, sales;
-    string answer;
-    bool error;
+    double registerCash, price, cash, change, tax, total, subtotal = 0;
+    string answer, description, giftyn, message;
+    bool error = true, sales, cancel = false, order;
     
     luke();
     
     cout << "How much money is in the cash register?" << endl;
     cin >> registerCash;
+    cout << "What is the tax rate? (in percent)" << endl;
+    cin >> tax;
+    tax = tax/100;
+    
+    cout << "The first order." << endl << endl;
     
     do {
-        cout << "What is the price of the item?" << endl;
-        cin >> price;
-        price *= 1.055;
-        cout << "The final price is $" << price << endl;
-        
+        cancel = false;
         do {
+            cout << "What is the price of the item?" << endl;
+            cin >> price;
+            order1.listprices.push_back(price);
+            subtotal += price;
+            cout << "Item description: " << endl;
+            cin >> description;
+            order1.list.push_back(description);
+        
+            cout << "Is there another item? y or n (c to cancel the order)" << endl;
+            cin >> answer;
+            if ("y" == answer || "Y" == answer) {
+                sales = true;
+            } else if ("c" == answer || "C" == answer) {
+                cancel = true;
+            } else {
+                sales = false;
+            }
+        } while (sales && !cancel);
+        
+        if (!cancel) {
+            cout << "Do you want to include a gift message? y or n" << endl;
+            cin >> giftyn;
+            if ("y" == giftyn || "Y" == giftyn) {
+                cout << "Enter your message:" << endl;
+                cin >> message;
+                strcpy(order1.gmessage, message);
+            }
+            
+            
+            for (int ii = 0; ii < order1.list.size(); ii++) {
+                cout << endl << order1.list[ii] << endl << order1.listprices[ii] << endl << "Gift Message:" << endl << order1.gmessage << endl;
+            }
+            tax *= subtotal;
+            total = subtotal + tax;
+            cout << "Subtotal:\t" << subtotal << endl;
+            cout << "Tax:\t" << tax << endl;
+            cout << "Total:\t" << total << endl;
+        }
+        
+        while (error && !cancel) {
             cout << "Input the customer's cash payment:" << endl;
             cin >> cash;
-            change = cash - price;
-            
+            //If you input anything other than an integer it creates an infinite loop. Why is cin not working?
+            change = cash - total;
+        
             if (cash < price) {
                 cerr << "Not enough payment!" << endl;
                 error = true;
@@ -38,17 +87,16 @@ int main (int argc, char *argv[])
                 registerCash += price;
                 error = false;
             }
-        } while (error);
-        
-        cout << "Is there another item? y or n" << endl;
-        cin >> answer;
-        if ("y" == answer || "Y" == answer) {
-            sales = true;
-        } else {
-            sales = false;
         }
         
-    } while (sales);
+        cout << "Is there another order? y or n" << endl;
+        cin >> answer;
+        if ("y" == answer || "Y" == answer) {
+            order = true;
+        } else {
+            order = false;
+        }
+    } while (order);
     
     cout << "The final amount of money in the cash register is $" << registerCash << endl;
     
